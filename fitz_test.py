0192d6064906c56d8f4628f86e0936ad3873c02f
zcvf <zcvf@users.noreply.github.com>
2022-05-19 23:02:57 +0300

    Update fitz_test.py

--- fitz_test.py
+++ fitz_test.py
@@ -18,9 +18,10 @@ import tkinter
 import tkinter.filedialog
 import os
 
-class SdPdfViewer:
+class SdPdfViewer():
 
-    def __init__(self):
+    def __init__(self, root):
+        self.root = root
         self.img_ref = None
         self.but_ref = None
         self.but = None
@@ -41,20 +42,20 @@ class SdPdfViewer:
 
     def open_file(self):
         self.file_for_open = tkinter.filedialog.askopenfilename(title="Open file",initialdir=(os.path.expanduser("~/")), defaultextension='.pdf')
-        self.load_pdf_page(root, 0)
-        self.load_pdf_preview(root, 0)
+        self.load_pdf_page(0)
+        self.load_pdf_preview(0)
 
         return
 
 
     def close_program(self):
-        root.quit()
+        self.root.quit()
         return
 
 
-    def add_menubar(self, root):
-        menubar = tkinter.Menu(root)
-        root.config(menu=menubar)
+    def add_menubar(self):
+        menubar = tkinter.Menu(self.root)
+        self.root.config(menu=menubar)
         filemenu = tkinter.Menu(menubar, tearoff=0)
         filemenu.add_command(label="Open...", command=self.open_file)
         filemenu.add_separator()
@@ -68,7 +69,7 @@ class SdPdfViewer:
 
     # function for scrolling pages of document on mouse scroll
     def mouse_wheel(self, event):
-        print("cur_page_num = %f" % self.cur_page_num)
+#        print("cur_page_num = %f" % self.cur_page_num)
         if event.num == 5 or event.delta < 0:
             print("scroll down")
             self.canvas.yview("scroll",1,"units")
@@ -76,8 +77,8 @@ class SdPdfViewer:
                 if self.cur_page_num < self.doc.page_count:
                     new_page = self.cur_page_num+1
                     self.cur_page_num = new_page
-                    self.load_pdf_page(root,new_page)
-                    root.after(100)
+                    self.load_pdf_page(new_page)
+                    self.root.after(100)
                     self.canvas.yview("moveto","0.0")
                     self.canvas.config(yscrollcommand=self.vbar.set(0.01,0.02))
         elif event.num == 4 or event.delta > 0:
@@ -87,8 +88,8 @@ class SdPdfViewer:
                 if self.cur_page_num > 0:
                     new_page = self.cur_page_num-1
                     self.cur_page_num = new_page
-                    self.load_pdf_page(root,new_page)
-                    root.after(100)
+                    self.load_pdf_page(new_page)
+                    self.root.after(100)
                     self.canvas.yview("moveto","1.0")
                     self.canvas.config(yscrollcommand=self.vbar.set(0.99,0.98))
 
@@ -99,15 +100,15 @@ class SdPdfViewer:
             self.canvas2.yview("scroll",-1,"units")
 
 
-    def load_pdf_page(self, root, page_num = 2, dpi = 120):
+    def load_pdf_page(self, page_num = 2, dpi = 120):
         self.cur_page_num = page_num
-        rw = root.winfo_screenwidth()
-        rh = root.winfo_screenheight()
+        rw = self.root.winfo_screenwidth()
+        rh = self.root.winfo_screenheight()
         if self.frame != None:
             print("frame in globals")
         else:
             print("frame not in globals")
-            self.frame = tkinter.Frame(root)
+            self.frame = tkinter.Frame(self.root)
             self.frame.pack(side=LEFT, expand=True, fill=BOTH, pady=0, padx = 0)
 
         if self.canvas != None:
@@ -134,10 +135,10 @@ class SdPdfViewer:
         self.tkimg = ImageTk.PhotoImage(img)
 
 
-        rw = root.winfo_width()
-        rh = root.winfo_height()
-        w = int(root.winfo_width()) - 300
-        h = int(root.winfo_height()) - 300
+        rw = self.root.winfo_width()
+        rh = self.root.winfo_height()
+        w = int(self.root.winfo_width()) - 300
+        h = int(self.root.winfo_height()) - 300
 
         if self.canvas != None:
             self.canvas.config(scrollregion = (0,0,pw,ph))
@@ -150,49 +151,46 @@ class SdPdfViewer:
             self.canvas.pack(side=LEFT,expand=True,fill=BOTH)
 
 
-'''
-
-    def load_pdf_preview(root, page_num = 2, dpi = 16):
-        global frame2, frame3, canvas2, doc, file_for_open
-        doc = fitz.Document(file_for_open)
-        rw = root.winfo_screenwidth()
-        rh = root.winfo_screenheight()
-        if frame2 != None:
-            print("frame2 in globals")
-            frame2.pack_forget()
-            frame2.destroy()
-            frame2=ttk.Frame(root)
-            frame2.config(width=200,height=frame2.winfo_height())
+    def load_pdf_preview(self, page_num = 2, dpi = 16):
+        self.doc = fitz.Document(self.file_for_open)
+        rw = self.root.winfo_screenwidth()
+        rh = self.root.winfo_screenheight()
+        if self.frame2 != None:
+            print("self.frame2 in globals")
+            self.frame2.pack_forget()
+            self.frame2.destroy()
+            self.frame2=ttk.Frame(self.root)
+            self.frame2.config(width=200,height=self.frame2.winfo_height())
 
         else:
-            frame2=ttk.Frame(root)
-            frame2.config(width=500,height=frame2.winfo_height())
-        if canvas2 != None:
-            print("canvas2 in globals")
-            canvas2.destroy()
-            canvas2 = tkinter.Canvas(frame2, bg="#dedede")
+            self.frame2=ttk.Frame(self.root)
+            self.frame2.config(width=500,height=self.frame2.winfo_height())
+        if self.canvas2 != None:
+            print("self.canvas2 in globals")
+            self.canvas2.destroy()
+            self.canvas2 = tkinter.Canvas(self.frame2, bg="#dedede")
         else:
-            canvas2 = tkinter.Canvas(frame2, bg="#dedede")
+            self.canvas2 = tkinter.Canvas(self.frame2, bg="#dedede")
         
-        print(" :: %d" % int(frame2.winfo_height()))
-        canvas2.config(width = 300, height = rh)
+        print(" :: %d" % int(self.frame2.winfo_height()))
+        self.canvas2.config(width = 300, height = rh)
 
-        if frame3 != None:
-            frame3.pack_forget()
-            frame3.destroy()
-            frame3 = ttk.Frame(canvas2)
-            frame3.bind(
+        if self.frame3 != None:
+            self.frame3.pack_forget()
+            self.frame3.destroy()
+            self.frame3 = ttk.Frame(self.canvas2)
+            self.frame3.bind(
                 "<Configure>",
-                lambda e: canvas2.configure(
-                    scrollregion=canvas2.bbox("all")
+                lambda e: self.canvas2.configure(
+                    scrollregion=self.canvas2.bbox("all")
                 )
             )
         else:
-            frame3 = ttk.Frame(canvas2)
-            frame3.bind(
+            self.frame3 = ttk.Frame(self.canvas2)
+            self.frame3.bind(
                 "<Configure>",
-                lambda e: canvas2.configure(
-                    scrollregion=canvas2.bbox("all")
+                lambda e: self.canvas2.configure(
+                    scrollregion=self.canvas2.bbox("all")
                 )
             )
 
@@ -200,69 +198,56 @@ class SdPdfViewer:
         preview = []
         preview.clear()
 
-        for i in range(0,doc.page_count):
-            prev_page = doc.load_page(i)
+        for i in range(0,self.doc.page_count):
+            prev_page = self.doc.load_page(i)
             preview.append(prev_page.get_pixmap(dpi=dpi)) 
 
 
         counter = 0
-        global img_ref
-        img_ref = []
+        self.img_ref = []
         for prev_img in preview:
             prev_w = prev_img.width
             prev_h = prev_img.height
             mode = "RGBA" if prev_img.alpha else "RGB"
             preview_img = Image.frombytes(mode, [prev_w, prev_h], prev_img.samples)
             preview_tkimg = ImageTk.PhotoImage(preview_img)
-            img_ref.append(preview_tkimg)
-            but = ttk.Button(frame3,image=img_ref[counter], command=lambda root=root, counter=counter: load_pdf_page(root, page_num = counter)) #, relief = 'flat'
+            self.img_ref.append(preview_tkimg)
+            but = ttk.Button(self.frame3,image=self.img_ref[counter], command=lambda counter=counter: self.load_pdf_page(page_num = counter)) #, relief = 'flat'
             but.pack()
             counter += 1
 
-        frame3.update_idletasks()
-        canvas2.update_idletasks()
-        frame2.update_idletasks()
+        self.frame3.update_idletasks()
+        self.canvas2.update_idletasks()
+        self.frame2.update_idletasks()
 
-        canvas2.bind("<Button-4>", mouse_wheel_preview)
-        canvas2.bind("<Button-5>", mouse_wheel_preview)
-        canvas2.bind("<MouseWheel>", mouse_wheel_preview)
+        self.canvas2.bind("<Button-4>", self.mouse_wheel_preview)
+        self.canvas2.bind("<Button-5>", self.mouse_wheel_preview)
+        self.canvas2.bind("<MouseWheel>", self.mouse_wheel_preview)
 
-        frame3.pack(side=RIGHT, expand=True, fill=Y)
+        self.frame3.pack(side=RIGHT, expand=True, fill=Y)
 
-        canvas2.pack(side=LEFT, expand=True, fill=Y)
+        self.canvas2.pack(side=LEFT, expand=True, fill=Y)
 
-        canvas2.create_window((1,1), window=frame3,anchor="nw")
+        self.canvas2.create_window((1,1), window=self.frame3,anchor="nw")
 
-        frame2.pack(side=RIGHT, expand=False, fill=Y)
+        self.frame2.pack(side=RIGHT, expand=False, fill=Y)
 
-        vbar2 = ttk.Scrollbar(frame2,orient=VERTICAL,command=canvas2.yview)
-        canvas2.config(yscrollcommand=vbar2.set)
-        vbar2.pack(side=RIGHT, expand=True, fill="y")
+        self.vbar2 = ttk.Scrollbar(self.frame2,orient=VERTICAL,command=self.canvas2.yview)
+        self.canvas2.config(yscrollcommand=self.vbar2.set)
+        self.vbar2.pack(side=RIGHT, expand=True, fill="y")
 
-'''
 
-img_ref = None
-but_ref = None
-but = None
-tkimg = None
-file_for_open = None
-frame = None
-canvas = None
-frame2 = None
-frame3 = None
-canvas2 = None
-doc = None
-hbar = None
-vbar = None
 
-root = tkinter.Tk()
-rw = root.winfo_screenwidth()
-rh = root.winfo_screenheight()
 
-root.geometry(f"{rw}x{rh}")
-root.title("PDF viewer by sd")
+if __name__ == "__main__":
+    root = tkinter.Tk()
+    rw = root.winfo_screenwidth()
+    rh = root.winfo_screenheight()
 
-pdf_viewer = SdPdfViewer()
-pdf_viewer.add_menubar(root)
+    root.geometry(f"{rw}x{rh}")
+    root.title("PDF viewer by sd")
 
-root.mainloop()
+    pdf_viewer = SdPdfViewer(root)
+    pdf_viewer.add_menubar()
+
+    root.mainloop()
